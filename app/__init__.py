ddd6020dbf7b1d261bef79035676e953ab53ed99
gramistella <gramistella@icloud.com>
2020-01-20 15:50:59 +0100

    Oops

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-import os
+import os, sys
 
-template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-template_dir = os.path.join(template_dir, 'yes-u\\website')
-
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 login = LoginManager(app)
