bc9c2d4ed3a9a30532fe34875bb0386a4b3a0264
gramistella <gramistella@icloud.com>
2020-04-15 14:27:10 +0200

    Added some debug information to the console log

--- app/static/js/your-page.js
+++ app/static/js/your-page.js
@@ -30,7 +30,16 @@ $(function() {
             'Access-Control-Allow-Methods' : 'POST'
         },
         uploadprogress: function(file, progress, bytesSent) {
-            console.log(bytesToSize(bytesSent));
+
+            var width = $(file.previewElement).find('span.dz-upload').width();
+            var parentWidth = $(file.previewElement).find('span.dz-upload').offsetParent().width(); // this will return parent element's width which also can be replaced with docuent to get viewport width
+            var current_percent = Math.round(100*width/parentWidth);
+            var progress_percent = Math.round(100*bytesSent/file.size);
+
+            if (progress_percent > current_percent){
+                $(file.previewElement).find('span.dz-upload').css('width', progress_percent+'%');
+                console.log('Upload @ ' + progress_percent + '%');
+            }
         }
     });
 });
