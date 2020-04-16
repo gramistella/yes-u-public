17c9a36dfe0d826ec8e1f9c3fca9247b6423accc
gramistella <gramistella@icloud.com>
2020-04-16 21:47:38 +0200

    Added gzip compression to static files

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_misaka import Misaka
 from flask_static_compress import FlaskStaticCompress
+from flask_compress import Compress
 
 app = Flask(__name__)
 if __name__ == '__main__':
@@ -12,6 +13,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 markdown = Misaka().init_app(app)
-compress = FlaskStaticCompress(app)
+flask_static_compress = FlaskStaticCompress(app)
+Compress().init_app(app)
 
 from app import routes
