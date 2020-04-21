099bdee55f90e97c74f1829f5326b3538288619c
gramistella <gramistella@icloud.com>
2020-04-21 11:51:17 +0200

    Disabling hover effects for touch devices

--- app/static/js/navigation.js
+++ app/static/js/navigation.js
@@ -41,3 +41,26 @@ for (i = 0; i < coll.length; i++) {
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
     console.log('changed!!');
 })
+
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+           || navigator.maxTouchPoints > 0
+           || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all the :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
